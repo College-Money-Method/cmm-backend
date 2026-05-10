00979b4f7fec6c186db3eb6dfe75915a8aa09099
vu.nguyen <vu.nguyen@collegemoneymethod.com>
2026-05-10 23:18:14 +0700

    fix: update download task def

--- .github/scripts/download-task-definition.sh
+++ .github/scripts/download-task-definition.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 aws ecs describe-task-definition \
   --task-definition "cmm-$ENV-backend" \
   --query taskDefinition \
+  | jq 'del(.taskDefinitionArn,.revision,.status,.registeredAt,.registeredBy,.deregisteredAt,.requiresAttributes,.compatibilities)' \
   > task-definition.json
